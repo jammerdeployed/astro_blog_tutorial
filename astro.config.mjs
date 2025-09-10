9f6ead0dee3162d6eeeed5c01936f272cfa980d9
Tom <chytiltomik@seznam.cz>
2025-09-10 18:32:52 +0200

    5.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://dynamic-bonbon-3c3d44.netlify.app/"
+});
